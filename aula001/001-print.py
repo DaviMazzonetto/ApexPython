88bfde81fb33a58def0bef1c2a5bdc29493c372c
Davi Ferreira Mazzonetto <70709340+DaviMazzonetto@users.noreply.github.com>
2022-08-22 21:35:26 -0300

    Delete aula001 directory

--- aula001/001-print.py
+++
@@ -1,9 +0,0 @@
-##Print escreve
-print('Hello, World!')
-##  \t tab
-print("\tDavi Mazzonetto")
-##  \n quebra linha
-print("Olá, Davi!\nSeja bem-vindo")
-##  * multiplica caractere
-print('-'*10, 'sistema de vendas', '-'*10)
-
--- aula001/002-input.py
+++
@@ -1,6 +0,0 @@
-## INPUT: retirar informaçoes do terminal
-nome = input('Digite seu nome:')
-print('Olá,',nome)
-
-sobrenome = input('Digite seu sobrenome')
-print = ('Seu sobrenome é:', sobrenome)
