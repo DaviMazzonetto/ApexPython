211663dbc3320749a2f9cffbfd46a9eb1d3d02d0
Davi Ferreira Mazzonetto <70709340+DaviMazzonetto@users.noreply.github.com>
2022-08-22 21:35:18 -0300

    Delete Aula003 directory

--- Aula003/002-estruturas de repeticaoP2.py
+++
@@ -1,9 +0,0 @@
-# Estruturas de repetição parte 2
-
-lista = [10,20,12,23,50]
-
-for i in lista: #percorre cada elemento da lista
-    print(i) 
-
-for i in 'Davi': #Percorre cada letra do String
-    print(i)
